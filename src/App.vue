d5018c692d6955de6933c22347d572e973593793
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-15 10:47:40 +0200

    Add Comments

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,18 @@
 </template>
 
 <script>
+/*
+
+1- Header
+2- Jumbotron (menu-bar)
+3- Services Section (card -> logos svg -> hover effects)
+4- Company Section (2 col background + position + dots effect)
+5- Projects Section (menu-link + card)
+6- Results Section (bgc-img + opacity + row-column)
+7- Touch Section (2 col -> form / address)
+8- Footer (2 col-> address / footer-link-menu + copyright)
+
+*/
 import Header from "@/components/Header.vue";
 import Main from "@/components/Main.vue";
 import Footer from "@/components/Footer.vue";
