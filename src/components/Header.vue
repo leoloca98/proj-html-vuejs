4162338789d6d6435d42354e05666b24b7a07db1
leoloca98 <leonardolocatelli98@gmail.com>
2021-09-16 19:00:12 +0200

    Complete Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,59 @@
 <template>
   <header>
-    <div class="info-menu">
-      <div class="general-container"></div>
+    <div class="info-menu text-white">
+      <div class="general-container">
+        <div class="row justify-content-between">
+          <div class="col-7 hours">
+            <span
+              ><i class="fas fa-clock"></i> Open Hours: Mon-Sat - 9.00 -
+              18.00</span
+            >
+          </div>
+          <div class="col-5 info d-flex justify-content-between">
+            <span><i class="fas fa-phone-alt"></i> +1 (305) 1234-5678</span>
+            <span><i class="fas fa-envelope"></i> hello@example.com</span>
+            <span><i class="fab fa-facebook-f"></i></span>
+            <span><i class="fab fa-twitter"></i></span>
+            <span><i class="fab fa-linkedin-in"></i></span>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="jumbotron">
-      <div class="general-container">
+      <div class="general-container h-100">
         <div
           class="
             row
-            m-0
-            p-0
-            flex-column
-            justify-content-center
+            top-bar-menu
+            justify-content-between
             align-items-center
+            pt-3
           "
         >
-          <div class="top-bar-menu"></div>
-          <div class="square"></div>
+          <div class="col-4">
+            <img src="../assets/img/logo.png" alt="Logo" />
+          </div>
+          <div class="col-8 d-flex justify-content-between">
+            <li>HOME</li>
+            <li>SERVICE</li>
+            <li>ABOUT</li>
+            <li>PROJECTS</li>
+            <li>RESULTS</li>
+            <li><span class="button-touch">GET IN TOUCH</span></li>
+          </div>
+        </div>
+        <div class="row something justify-content-center align-items-center">
+          <div class="col team text-center">
+            <h1 class="pb-4 fs-h1">
+              Ready <span class="principal-color effect-color">Team</span>
+            </h1>
+            <p class="pb-5">
+              No matter what your company needs, we will be ready to assist you
+              in the best possible way
+            </p>
+            <span class="me-4 button-touch">GET IN TOUCH</span>
+            <span class="ms-4 button-empty">READ MORE</span>
+          </div>
         </div>
       </div>
     </div>
@@ -38,6 +75,7 @@ header {
   .info-menu {
     height: 5vh;
     width: 100%;
+    line-height: 5vh;
     background-color: $HeaderBg;
   }
 
@@ -50,15 +88,18 @@ header {
     .top-bar-menu {
       height: 55px;
       width: 100%;
-      background-color: forestgreen;
-      margin-bottom: 250px;
+
+      img {
+        max-height: 40px;
+      }
     }
+    .something {
+      height: calc(85vh - 55px);
 
-    .square {
-      height: 250px;
-      width: 450px;
-
-      background-color: goldenrod;
+      .team {
+        height: 250px;
+        max-width: 450px;
+      }
     }
   }
 }
